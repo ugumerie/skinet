cc0bcc5e76ee6dbb474f9dc61fbc544181e89f15
Ugochukwu Umerie <upchibuzz@gmail.com>
2021-01-10 22:53:00 +0100

    End of section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,11 +12,13 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
+  quantity = 1;
 
   constructor(
     private shopService: ShopService,
     private activatedRoute: ActivatedRoute,
-    private bcService: BreadcrumbService
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
   ) {
     // set to an empty content when this component is being mounted
     this.bcService.set('@productDetails', ' ');
@@ -25,11 +28,25 @@ export class ProductDetailsComponent implements OnInit {
     this.loadProduct();
   }
 
+  addItemToBasket(): void {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity(): void {
+    this.quantity++;
+  }
+
+  decrementQuantity(): void {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
   loadProduct(): void {
     this.shopService
       .getProduct(+this.activatedRoute.snapshot.paramMap.get('id'))
       .subscribe(
-        (product) => {
+        (product: IProduct) => {
           this.product = product;
           // the alias is set at shop-route-module, u assign a value to the alias like so
           this.bcService.set('@productDetails', product.name);
